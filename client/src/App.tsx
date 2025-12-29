55dc448132de00a572ddf8736ffb4a58df5401ee
manish <manishmahto198@gmail.com>
2025-12-30 00:34:31 +0530

    update env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 import "./App.css";
 
-// const apiURL = `${import.meta.env.VITE_API_URL}/api/message`
-const apiURL = `http://3.110.224.177:4000/api/message`
+const apiURL = `${import.meta.env.VITE_API_URL}/api/message`
+// const apiURL = `http://3.110.224.177:4000/api/message`
 
 
 console.log({apiURL});
